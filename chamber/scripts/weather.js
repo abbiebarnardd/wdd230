1067cfb3ad33197688b95260ee824733b4331025
abbiebarnardd <55629678+abbiebarnardd@users.noreply.github.com>
2023-06-26 15:57:20 -0700

    update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,10 +2,10 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const speed = document.querySelector('#windSpeed');
 
-const api_key = "1373c7c17cddfb5ba77da2c14aa22802"
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=1373c7c17cddfb5ba77da2c14aa22802"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Mesquite&units=imperial&appid=1373c7c17cddfb5ba77da2c14aa22802"
 
 
 
@@ -27,8 +27,9 @@ async function apiFetch() {
   apiFetch();
 
   function displayResults(weatherData) {
-    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)} &deg;F`;
-  
+    currentTemp.innerHTML = `Temperature: <strong>${weatherData.main.temp.toFixed(0)} &deg;F</strong>`;
+    speed.innerHTML = `Wind Speed: ${weatherData.wind.speed.toFixed(0)} mph`;
+    
   
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
@@ -36,6 +37,20 @@ async function apiFetch() {
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
+
+    let t = weatherData.main.temp.toFixed(0);
+    let s = weatherData.wind.speed.toFixed(0);
+    let w = "";
+
+    if (t <=50 && s> 3) {
+      w = "Wind Chill: " + Math.round(35.74 + 0.6215 * t -35.75 * s **0.16 + 0.4275 * t * s ** 0.16);
+      document.getElementById("windChill").innerHTML = w.toString();
+    }
+
+    else {
+      w = "Wind Chill: N/A";
+      document.getElementById("windChill").innerHTML = w.toString();
+    }
   }
 
   displayResults();
