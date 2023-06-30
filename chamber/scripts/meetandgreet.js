47320a5a271ed715e1d325d32a6166d3bbba6388
abbiebarnardd <55629678+abbiebarnardd@users.noreply.github.com>
2023-06-29 22:28:44 -0700

    update

--- chamber/scripts/meetandgreet.js
+++ chamber/scripts/meetandgreet.js
@@ -1,3 +1,3 @@
-if (new Date().getDay() == 4 || new Date().getDay() == 2 ) {
+if (new Date().getDay() == 1 || new Date().getDay() == 2 ) {
     document.getElementById("chamber").style.display = "block"
 }
